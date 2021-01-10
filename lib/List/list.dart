191f4f0af6f4b53c46433dfe4b706c5d0cb2d2a3
haries hashemeen bin alias <harieshashemeen@gmail.com>
2021-01-11 03:46:55 +0800

    Shop screen v1

---
+++ lib/List/list.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+List<Map> shirts = [
+  {'name': 'Polo v1', 'brand': 'Polo Ralph', 'image': 'assets/images/four.jpg'},
+  {'name': 'Swagor', 'brand': 'Levis', 'image': 'assets/images/two.jpg'},
+  {'name': 'Munchik', 'brand': 'Guess', 'image': 'assets/images/three.jpg'},
+];
--- lib/screen/shop.dart
+++ lib/screen/shop.dart
@@ -1,3 +1,4 @@
+import 'package:closet_map/List/list.dart';
 import 'package:closet_map/nav_bar/CustomAppBar.dart';
 import 'package:flutter/material.dart';
 
@@ -8,21 +9,180 @@ class ShopScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.orangeAccent,
+      appBar: AppBar(
+        backgroundColor: Colors.orangeAccent,
+        title: new Center(child: new Text("SHOP", textAlign: TextAlign.center)),
+      ),
+      backgroundColor: Colors.white,
       bottomNavigationBar: CustomAppBar(),
-      body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
             children: <Widget>[
+              Container(
+                height: 40,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    AspectRatio(
+                        aspectRatio: 2.2 / 1,
+                        child: Container(
+                          margin: EdgeInsets.only(right: 10),
+                          decoration: BoxDecoration(
+                              color: Colors.grey[200],
+                              borderRadius: BorderRadius.circular(20)),
+                          child: Center(
+                            child: Text(
+                              "All",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ),
+                        )),
+                    AspectRatio(
+                        aspectRatio: 2.2 / 1,
+                        child: Container(
+                          margin: EdgeInsets.only(right: 10),
+                          child: Center(
+                            child: Text(
+                              "Mens",
+                              style: TextStyle(fontSize: 17),
+                            ),
+                          ),
+                        )),
+                    AspectRatio(
+                        aspectRatio: 2.2 / 1,
+                        child: Container(
+                          margin: EdgeInsets.only(right: 10),
+                          child: Center(
+                            child: Text(
+                              "Womens",
+                              style: TextStyle(fontSize: 17),
+                            ),
+                          ),
+                        )),
+                    AspectRatio(
+                        aspectRatio: 2.2 / 1,
+                        child: Container(
+                          margin: EdgeInsets.only(right: 10),
+                          child: Center(
+                            child: Text(
+                              "Kids",
+                              style: TextStyle(fontSize: 17),
+                            ),
+                          ),
+                        )),
+                    AspectRatio(
+                        aspectRatio: 2.2 / 1,
+                        child: Container(
+                          margin: EdgeInsets.only(right: 10),
+                          child: Center(
+                            child: Text(
+                              "Shoes",
+                              style: TextStyle(fontSize: 17),
+                            ),
+                          ),
+                        )),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              makeItem(
+                  names: shirts[0]['name'],
+                  image: shirts[0]['image'],
+                  brand: shirts[0]['brand'],
+                  context: context),
+              makeItem(
+                  names: shirts[1]['name'],
+                  image: shirts[1]['image'],
+                  brand: shirts[1]['brand'],
+                  context: context),
+              makeItem(
+                  names: shirts[2]['name'],
+                  image: shirts[2]['image'],
+                  brand: shirts[2]['brand'],
+                  context: context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget makeItem({names, image, brand, context}) {
+    return Hero(
+      tag: brand,
+      child: GestureDetector(
+        onTap: () {},
+        child: Container(
+          height: 250,
+          width: double.infinity,
+          padding: EdgeInsets.all(20),
+          margin: EdgeInsets.only(bottom: 20),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              image:
+                  DecorationImage(image: AssetImage(image), fit: BoxFit.cover),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey[400],
+                    blurRadius: 10,
+                    offset: Offset(0, 10))
+              ]),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          names,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          brand,
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: 35,
+                    height: 35,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle, color: Colors.white),
+                    child: Center(
+                      child: Icon(
+                        Icons.favorite_border,
+                        size: 20,
+                      ),
+                    ),
+                  )
+                ],
+              ),
               Text(
-                "Untuk Shop Screen",
+                "100\$",
                 style: TextStyle(
-                    fontSize: 32.0,
                     color: Colors.white,
-                    fontFamily: 'Oxygen',
+                    fontSize: 30,
                     fontWeight: FontWeight.bold),
               ),
-            ]),
+            ],
+          ),
+        ),
       ),
     );
   }
