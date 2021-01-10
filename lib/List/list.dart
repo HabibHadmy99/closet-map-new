51effabe55d377cfe87639083b0941a698bd8210
haries hashemeen bin alias <harieshashemeen@gmail.com>
2021-01-11 07:10:02 +0800

    cart screen v1

--- lib/List/list.dart
+++ lib/List/list.dart
@@ -5,3 +5,26 @@ List<Map> shirts = [
   {'name': 'Swagor', 'brand': 'Levis', 'image': 'assets/images/two.jpg'},
   {'name': 'Munchik', 'brand': 'Guess', 'image': 'assets/images/three.jpg'},
 ];
+
+List<Map> buyshirts = [
+  {
+    'name': 'Polo v1',
+    'brand': 'Polo Ralph',
+    'image': 'assets/images/four.jpg',
+    'price': '100'
+  },
+  {
+    'name': 'Munchik',
+    'brand': 'Guess',
+    'image': 'assets/images/three.jpg',
+    'price': '120'
+  },
+];
+
+List<Map> icon = [
+  {
+    'name': 'Polo v1',
+    'brand': 'Polo Ralph',
+    'image': 'assets/images/61848.png'
+  },
+];
--- lib/screen/cart.dart
+++ lib/screen/cart.dart
@@ -1,3 +1,4 @@
+import 'package:closet_map/List/list.dart';
 import 'package:closet_map/nav_bar/CustomAppBar.dart';
 import 'package:flutter/material.dart';
 
@@ -8,21 +9,101 @@ class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.orangeAccent,
+        title: Column(
+          children: [
+            Text(
+              "Your Cart",
+              style: TextStyle(color: Colors.black),
+            ),
+            Text(
+              "${buyshirts.length} items",
+              style: Theme.of(context).textTheme.caption,
+            ),
+          ],
+        ),
+      ),
       bottomNavigationBar: CustomAppBar(),
-      backgroundColor: Colors.orangeAccent,
-      body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                "Untuk Cart Screen",
-                style: TextStyle(
-                    fontSize: 32.0,
-                    color: Colors.white,
-                    fontFamily: 'Oxygen',
-                    fontWeight: FontWeight.bold),
+      backgroundColor: Colors.white,
+      body: Container(
+        //height: 400,
+        child: ListView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: buyshirts.length,
+          itemBuilder: (context, index) {
+            return Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(height: 20),
+                  Container(
+                    child: Row(children: [
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 16),
+                        width: 150,
+                        height: 150,
+                        decoration: BoxDecoration(
+                          color: Colors.red[100],
+                          image: new DecorationImage(
+                              image: new AssetImage(buyshirts[index]['image'])),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      /*Column(children: [
+                        Container(
+                          height: 50,
+                          padding: EdgeInsets.only(left: 10),
+                          child: Text(
+                            buyshirts[index]['name'],
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          height: 50,
+                          padding: EdgeInsets.only(left: 10),
+                          child: Text(
+                            buyshirts[index]['brand'],
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          //height: 120,
+                          padding: EdgeInsets.only(left: 10),
+                          child: Text(
+                            buyshirts[index]['price'],
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                      ]),
+                      Container(
+                        padding: EdgeInsets.all(30),
+                        width: 35,
+                        height: 35,
+                        decoration: BoxDecoration(
+                          color: Colors.red[500],
+                          image: new DecorationImage(
+                              image: new AssetImage("assets/images/61848.png")),
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      ),*/
+                    ]),
+                  ),
+                ],
               ),
-            ]),
+            );
+          },
+        ),
       ),
     );
   }
