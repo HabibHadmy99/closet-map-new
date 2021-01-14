186bef159fbd31df870e7ec4bd9b2777f6c1f548
haries hashemeen bin alias <harieshashemeen@gmail.com>
2021-01-14 16:04:08 +0800

    update checkout

--- lib/screen/Checkout.dart
+++ lib/screen/Checkout.dart
@@ -1,3 +1,4 @@
+import 'package:closet_map/List/list.dart';
 import 'package:flutter/material.dart';
 
 class Checkout extends StatelessWidget {
@@ -8,7 +9,30 @@ class Checkout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        elevation: 0.1,
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          color: Colors.black,
+          onPressed: () {
+            //Navigator.pop(context);
+          },
+        ),
+        title: Column(
+          children: [
+            Text(
+              "Checkout",
+              style: TextStyle(color: Colors.black),
+            ),
+            Text(
+              "${buyshirts.length} items",
+              style: Theme.of(context).textTheme.caption,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
