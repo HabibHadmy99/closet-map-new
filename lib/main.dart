b7fd4a7cf40b9557d98845c4dcdbfd48449adb68
mirame-lab <73416417+mirame-lab@users.noreply.github.com>
2021-01-15 18:38:14 +0800

    implemented dependency for items
    Sorry :(

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:closet_map/screen/admin/admin_home.dart';
 import 'package:flutter/material.dart';
-
+import 'dependencies.dart' as di;
 import 'screen/splash_screen.dart';
 
 void main() {
+  di.init();
   runApp(MyApp());
 }
 
