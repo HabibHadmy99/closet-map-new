0a91a1396ca0082d9a8c44670bd5fb3287d9cd19
HabibHadmy99 <74220113+HabibHadmy99@users.noreply.github.com>
2021-01-16 02:24:30 +0800

    Add user data service

---
+++ lib/Services/user_data_service.dart
@@ -0,0 +1,24 @@
+import 'package:closet_map/List/user.dart';
+
+abstract class UserDataService {
+  Future<User> signin(String email, String password);
+  Future signout();
+  Future newSignup({
+    String name,
+    String email,
+    String contact,
+    String address,
+    String password,
+    String type,
+  });
+
+  Future<User> getCurrentUser();
+  Future updateUser({
+    String name,
+    String email,
+    String contact,
+    String address,
+    String password,
+  });
+  Future<List<User>> getUserList();
+}
