decaec65b5be1cf273e894e5692f5c1560b08ebb
HabibHadmy99 <74220113+HabibHadmy99@users.noreply.github.com>
2021-01-16 02:29:29 +0800

    Update dependencies.dart

--- lib/dependencies.dart
+++ lib/dependencies.dart
@@ -1,4 +1,6 @@
 import 'package:closet_map/Services/OrderService/order_data_service_mock.dart';
+import 'package:closet_map/Models/mock_user.dart';
+import 'package:closet_map/Services/user_data_service.dart';
 import 'package:get_it/get_it.dart';
 
 import 'Services/items_service.dart';
@@ -8,4 +10,5 @@ GetIt service = GetIt.instance;
 void init() {
   service.registerLazySingleton(() => OrderDataServiceMock());
   service.registerLazySingleton(() => ItemsDataServiceMock());
+  service.registerLazySingleton<UserDataService>(() => UserDataServiceMock());
 }
