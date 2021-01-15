66e70bc1700e2f5e07216ab6462edbf79710874e
mirame-lab <73416417+mirame-lab@users.noreply.github.com>
2021-01-15 18:16:35 +0800

    implemented dependency to shop screen and admin home

--- lib/screen/admin/admin_home.dart
+++ lib/screen/admin/admin_home.dart
@@ -1,19 +1,36 @@
 
 import 'package:closet_map/List/list.dart';
+import 'package:closet_map/Models/item_model.dart';
+import 'package:closet_map/Services/items_service.dart';
 import 'package:closet_map/nav_bar/CustomAppBar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../../dependencies.dart';
 import '../home.dart';
 import '../shop.dart';
 import 'admin_changeDetails.dart';
 
 
 class AdminHomeScreen extends StatelessWidget {
+   List<Items> _items;
   static Route<dynamic> route() =>
       MaterialPageRoute(builder: (_) => AdminHomeScreen());
 
   @override
   Widget build(BuildContext context) {
+    final ItemsDataServiceMock todoDataService = service();
+
+    return FutureBuilder<List<Items>>(
+        future: todoDataService.getItemsList(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            _items = snapshot.data;
+            return _buildAdminHome();
+          }
+          return _buildFetchingDataScreen();
+        });
+  }
+  Scaffold _buildAdminHome() {
     return Scaffold(
       bottomNavigationBar: CustomAppBar(),
       body: Column(
@@ -29,19 +46,20 @@ class AdminHomeScreen extends StatelessWidget {
           ),
           Expanded(
             child: ListView.builder(
-              itemCount: shirts.length,
+              itemCount:_items.length,
               itemBuilder: (context, index){
+                final _item = _items[index];
                 return ListTile(
                   contentPadding: EdgeInsets.only(left: 0),
                   title: Text(
-                    itemlist[index].name,
+                    _item.name,
                     style: TextStyle(letterSpacing: 1),
                   ),
                   leading: Container(
                       width: 100,
                       height:80,
                       child: Image(
-                        image: AssetImage(itemlist[index].image,),
+                        image: AssetImage(_item.image,),
                         fit: BoxFit.cover), 
                       ),
                   
@@ -50,7 +68,7 @@ class AdminHomeScreen extends StatelessWidget {
                     Navigator.push(
                        context,
                        MaterialPageRoute(
-                        builder: (context) => ItemChangeForm(itemsList: itemlist[index]),
+                        builder: (context) => ItemChangeForm(itemsList: _item),
                     ),
                   );
                   },
@@ -64,6 +82,20 @@ class AdminHomeScreen extends StatelessWidget {
         child: Icon(Icons.add),
         backgroundColor: Colors.pinkAccent,
         onPressed: () {},
+      ),
+    );
+  }
+  Scaffold _buildFetchingDataScreen() {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircularProgressIndicator(),
+            SizedBox(height: 50),
+            Text('Loading items...'),
+          ],
+        ),
       ),
     );
   }
