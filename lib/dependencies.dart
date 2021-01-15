886dc2d59ac6383f101c3196fe88cef4da786e73
mirame-lab <73416417+mirame-lab@users.noreply.github.com>
2021-01-15 17:12:27 +0800

    Update dependencies.dart

--- lib/dependencies.dart
+++ lib/dependencies.dart
@@ -1,8 +1,11 @@
 import 'package:closet_map/Services/OrderService/order_data_service_mock.dart';
 import 'package:get_it/get_it.dart';
 
+import 'Services/items_service.dart';
+
 GetIt service = GetIt.instance;
 
 void init() {
   service.registerLazySingleton(() => OrderDataServiceMock());
+  service.registerLazySingleton(() => ItemsDataServiceMock());
 }
