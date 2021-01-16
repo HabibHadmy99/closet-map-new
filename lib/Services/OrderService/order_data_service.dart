cae0a59d5ef9d81da3f4b43ad2a5dc7e3c7ec56f
haries hashemeen bin alias <74220427+hariesGit@users.noreply.github.com>
2021-01-16 21:42:05 +0800

    updated orderservices

---
+++ lib/Services/OrderService/order_data_service.dart
@@ -0,0 +1,7 @@
+import 'package:closet_map/Models/item_model.dart';
+
+abstract class OrderDataService {
+  Future<List<Items>> getOrderList();
+  Future<Items> updateCartlist({Items items});
+  Future deleteItems({int id});
+}
---
+++ lib/Services/OrderService/order_data_service_mock.dart
@@ -0,0 +1,50 @@
+import 'package:closet_map/List/list.dart';
+import 'package:closet_map/Models/item_model.dart';
+
+import 'order_data_service.dart';
+
+final _itemlistDatabase2 = <Items>[
+  Items(
+    id: 1,
+    name: 'Polo v1',
+    brand: 'Polo Ralph',
+    image: 'assets/images/four.jpg',
+    quantity: 6,
+    price: 100,
+    desc: 'This is item description for Polo v1',
+  ),
+  Items(
+    id: 2,
+    name: 'Swagor',
+    brand: 'Levis',
+    image: 'assets/images/two.jpg',
+    quantity: 6,
+    price: 100,
+    desc: 'This is item description for Swagor',
+  ),
+  Items(
+    id: 3,
+    name: 'Munchik',
+    brand: 'Guess',
+    image: 'assets/images/three.jpg',
+    quantity: 6,
+    price: 120,
+    desc: 'This is item description for Munchik',
+  ),
+];
+
+class OrderDataServiceMock implements OrderDataService {
+  Future<List<Items>> getOrderList() async {
+    return [..._itemlistDatabase2];
+  }
+
+  Future<Items> updateCartlist({Items items}) async {
+    _itemlistDatabase2.add(items);
+    return items;
+  }
+
+  // ignore: missing_return
+  Future deleteItems({int id}) {
+    _itemlistDatabase2.removeWhere((items) => items.id == id);
+  }
+}
