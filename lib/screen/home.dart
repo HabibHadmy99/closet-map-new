125d9125ac9de13c446a37786ffca86fbe625300
HabibHadmy99 <habib1999@graduate.utm.my>
2021-01-16 15:21:09 +0800

    Update home.dart

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,17 +1,21 @@
 import 'package:closet_map/List/list.dart';
 import 'package:closet_map/List/user.dart';
 import 'package:closet_map/Models/item_model.dart';
+import 'package:closet_map/Services/items_dataservice.dart';
 import 'package:closet_map/Services/items_service.dart';
+import 'package:closet_map/screen/shop.dart';
 
 import 'package:flutter/cupertino.dart';
 import 'package:closet_map/nav_bar/CustomAppBar.dart';
 import 'package:closet_map/shape_clipper/Shape4HomeTop.dart';
 import 'package:flutter/material.dart';
+import '../dependencies.dart';
 import 'account.dart';
 import 'signin.dart';
 
 List<String> setupOption = ['User Setting', 'Logout'];
 User user;
+List<Items> _items;
 
 class Home extends StatelessWidget {
   static Route<dynamic> route(current) {
@@ -144,6 +148,20 @@ class HomeBottomScreen extends StatefulWidget {
 class HomeBottomScreenState extends State<HomeBottomScreen> {
   @override
   Widget build(BuildContext context) {
+    final ItemsDataServiceMock todoDataService = service();
+
+    return FutureBuilder<List<Items>>(
+        future: todoDataService.getItemsList(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            _items = snapshot.data;
+            return _buildBottomPart();
+          }
+          return _buildFetchingDataScreen();
+        });
+  }
+
+  Widget _buildBottomPart() {
     return Column(
       children: <Widget>[
         Padding(
@@ -164,23 +182,192 @@ class HomeBottomScreenState extends State<HomeBottomScreen> {
             ],
           ),
         ),
+        SingleChildScrollView(
+            child: Container(
+                height: 425.0,
+                child: ListView.builder(
+                    scrollDirection: Axis.vertical,
+                    itemCount: _items.length,
+                    itemBuilder: (context, index) {
+                      final _item = _items[index];
+                      return Container(
+                        child: MakeItem(
+                          brand: _item.brand,
+                          image: _item.image,
+                          context: context,
+                          names: _item.name,
+                          price: _item.price,
+                          index: index,
+                          items: _item,
+                        ),
+                      );
+                    }))
+            /*
         Container(
-          height: 250.0,
+          height: 180.0,
           child: ListView(scrollDirection: Axis.horizontal, children: <Widget>[
-            Container(
-              height: 210.0,
-              width: 100.0,
-              //color: Colors.amberAccent,
+            Padding(padding: const EdgeInsets.symmetric(horizontal: 4.0)),
+            ClipRRect(
+              borderRadius: BorderRadius.all(
+                Radius.circular(10.0),
+              ),
+              child: Stack(children: <Widget>[
+                Container(
+                  height: 250.0,
+                  width: 250.0,
+                  child: Image.asset("assets/images/lasvegas.jpg",
+                      fit: BoxFit.cover),
+                ),
+                Positioned(
+                  left: 0.0,
+                  bottom: 0.0,
+                  width: 160.0,
+                  height: 60.0,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter,
+                            colors: [
+                          Colors.black,
+                          Colors.black.withOpacity(0.1),
+                        ])),
+                  ),
+                ),
+                Positioned(
+                  left: 10.0,
+                  bottom: 10.0,
+                  right: 10.0,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Name',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontSize: 18.0),
+                          ),
+                          Text(
+                            'Brand',
+                            style: TextStyle(
+                                fontWeight: FontWeight.normal,
+                                color: Colors.white,
+                                fontSize: 14.0),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 6.0, vertical: 2.0),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          shape: BoxShape.rectangle,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10.0),
+                          ),
+                        ),
+                        child: Text(
+                          'Harge',
+                          style: TextStyle(fontSize: 20.0, color: Colors.black),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 250.0,
+                  width: 350.0,
+                  child: Image.asset("assets/images/lasvegas.jpg",
+                      fit: BoxFit.cover),
+                ),
+                Positioned(
+                  left: 0.0,
+                  bottom: 0.0,
+                  width: 160.0,
+                  height: 60.0,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter,
+                            colors: [
+                          Colors.black,
+                          Colors.black.withOpacity(0.1),
+                        ])),
+                  ),
+                ),
+                Positioned(
+                  left: 10.0,
+                  bottom: 10.0,
+                  right: 10.0,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Name',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontSize: 18.0),
+                          ),
+                          Text(
+                            'Brand',
+                            style: TextStyle(
+                                fontWeight: FontWeight.normal,
+                                color: Colors.white,
+                                fontSize: 14.0),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 6.0, vertical: 2.0),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          shape: BoxShape.rectangle,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10.0),
+                          ),
+                        ),
+                        child: Text(
+                          'Harge',
+                          style: TextStyle(fontSize: 20.0, color: Colors.black),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ]),
             ),
-            Container(height: 210.0, width: 100.0),
-            Container(height: 210.0, width: 100.0),
-            Container(height: 210.0, width: 100.0),
-            Container(height: 210.0, width: 100.0)
           ]),
         ),
+      */
+            )
       ],
     );
+  }
 
-    //class productView
+  Widget _buildFetchingDataScreen() {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircularProgressIndicator(),
+            SizedBox(height: 50),
+            Text('Loading items...'),
+          ],
+        ),
+      ),
+    );
   }
 }
